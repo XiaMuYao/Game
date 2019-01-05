88a27ccc60bc1b9ce81c7396c47d2e55a8de7b8f
nelson <nelson2014@qq.com>
2019-01-05 10:42:26 +0800

    增加返回金币数量的接口

--- app/src/main/java/com/ydws/game/api/SecondApiService.kt
+++ app/src/main/java/com/ydws/game/api/SecondApiService.kt
@@ -1,9 +1,17 @@
 package com.ydws.game.api
 
-/***
- * Created by mo on 2019/1/5
- * 冷风如刀，以大地为砧板，视众生为鱼肉。
- * 万里飞雪，将穹苍作烘炉，熔万物为白银。
- **/
+import com.ydws.game.net.base.BaseResponse
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
 interface SecondApiService {
+    /**
+     * 充值时返回金币数量
+     */
+    @GET("agent/findBalance")
+    fun findBalance(@Query("userId") userId: String,
+              @Query("sessionId") sessionId: String,
+              @Query("language") language: String): Observable<BaseResponse<String>>
 }
