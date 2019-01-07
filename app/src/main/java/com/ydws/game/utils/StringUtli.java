0b662c130ec5820977e2fe344f3420658fbd11f1
XiaMuYao <XiaMuYaodqx@gmail.com>
2019-01-07 10:48:06 +0800

    增加了sessid的获取  改为动态申请

--- app/src/main/java/com/ydws/game/utils/StringUtli.java
+++ app/src/main/java/com/ydws/game/utils/StringUtli.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.telephony.TelephonyManager;
 
+import com.ydws.game.App;
 import com.ydws.game.BuildConfig;
 
 /**
@@ -18,19 +19,14 @@ import com.ydws.game.BuildConfig;
 public class StringUtli {
     /**
      * 去你妈
+     *
      * @return
      */
-    public static String getBlueTooth(){
-        BluetoothAdapter m_BluetoothAdapter = null; // Local Bluetooth adapter
-        m_BluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if(BuildConfig.DEBUG && m_BluetoothAdapter == null){
-            return "asdkljqwldjasdoiqwpoiddd";
-        }
-        String m_szBTMAC = m_BluetoothAdapter.getAddress();
-        if (null == m_szBTMAC || m_szBTMAC.isEmpty()){
-            m_szBTMAC = "askdklsdlasjdoasjud";
-        }
-        return m_szBTMAC;
+    public static String getBlueTooth() {
+        Context context = App.Companion.getInstance();
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = telephonyManager.getDeviceId();
+        System.out.println("imei========" + imei);
+        return imei;
     }
 }
